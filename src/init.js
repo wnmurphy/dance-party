d7f6d9c8252ae269f09ad8628101a7ecaa618fa0
Neil Murphy <wnmurphy@gmail.com>
2016-01-19 10:26:56 -0800

    Add lineUp button.

--- src/init.js
+++ src/init.js
@@ -25,11 +25,17 @@ $(document).ready(function(){
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
-      Math.random() * 1000;
+      Math.random() * 1000
     );
 
     window.dancers.push(dancer);
     $('body').append(dancer.$node);
   });
-
+  
+  $("#lineUpButton").on("click", function() {
+    for (var i = 0; i < window.dancers.length; i++) {
+      var dancer = window.dancers[i];
+      dancer.setPosition(top, 0);
+    }
+  });
 });
