0a546e9ad6e89de37e354bbcee09d5ecbe8c92ae
Neil Murphy <wnmurphy@gmail.com>
2016-01-19 08:14:37 -0800

    Catch-up commit; git w floobits not seeing file changes; deleted and cloned fresh repo

---
+++ src/FadeDancer.js
@@ -0,0 +1,12 @@
+var FadeDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="fadeDancer"></span>');
+  this.setPosition(top, left);
+};
+
+FadeDancer.prototype = Object.create(Dancer.prototype);
+FadeDancer.prototype.constructor = FadeDancer;
+FadeDancer.prototype.step = function(timeBetweenSteps){
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+  this.$node.fadeToggle();
+};
---
+++ src/spinDancer.js
@@ -0,0 +1,13 @@
+var SpinDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="spinDancer"></span>');
+  this.setPosition(top, left);
+  this.rotation = 0;
+};
+
+SpinDancer.prototype = Object.create(Dancer.prototype);
+SpinDancer.prototype.constructor = SpinDancer;
+SpinDancer.prototype.step = function(timeBetweenSteps){
+  Dancer.prototype.step.call(this, 20);
+  this.$node.css('transform', 'rotate(' + (this.rotation += 20) + 'deg)');
+}; 
